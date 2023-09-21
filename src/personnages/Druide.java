627f059271817ec1673f56a2c8b3a00968ca6bd6
Iwiish <elsa.daguerre81@gmail.com>
2023-09-21 15:25:07 +0200

    Import de random

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,9 +1,14 @@
 package personnages;
+import java.util.Random;
+
+import java.lang.reflect.Method;
 
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+	private int forcePotion = 1;
+	
 	
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
@@ -12,6 +17,10 @@ public class Druide {
 		
 		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
 	
+	}
+	
+	public void preparerPotion() {
+		
 	}
 
 	public String getNom() {
